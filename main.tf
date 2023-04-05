6bec3b9c4c8f4e6f909495dd79c30077cd3eb5fe
Hristo Trendafilov <hristo.trendafilov@softwareone.com>
2023-04-05 17:02:50 +0300

    Add credentials

--- main.tf
+++ main.tf
@@ -9,6 +9,11 @@ terraform {
 
 provider "azurerm" {
   features {}
+
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  subscription_id = var.subscription_id
 }
 
 resource "azurerm_policy_definition" "addTagToRG" {
--- variable.tf
+++ variable.tf
@@ -1,8 +1,23 @@
-variable "tag_list"{
-type   = list(string)
-description = "List of Mandatory tags"
+# Azure GUIDS
+variable "tenant_id" {
+  type = string
+  description = "Azure Tenant ID"
+}
+variable "client_id" {
+  type = string
+  description = "Azure Application ID"
+}
+variable "client_secret" {
+  type = string
+  sensitive = true
+  description = "Azure Application Secret"
 }
 variable "subscription_id" {
-    type = string
-    description = "Azure Subscription ID"
+  type = string
+  description = "Azure Subscription ID"
+}
+
+variable "tag_list"{
+  type   = list(string)
+  description = "List of Mandatory tags"
 }
